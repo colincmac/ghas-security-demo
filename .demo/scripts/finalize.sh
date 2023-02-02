074e3c654f5200353b1962c080909732d9e34c8c
Peter Murray <peter-murray@GitHub.com>
2023-02-02 16:35:44 +0000

    Adding finalizer script

---
+++ .demo/scripts/finalize.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+#
+# We can only do so much from Terraform with the initialization of the
+# repository. This script will perform some extra setup steps necessary
+# to initialize the repository state.
+#
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Encode JSON payload in base64 to prevent issues passing it through
+TERRAFORM_PARAMETERS_B64=`echo "${TERRAFORM_PARAMETERS}" | base64`
+
+docker run \
+  -v $DIR/ansible:/ansible \
+  -w /ansible \
+  -e WORKFLOW_GITHUB_DEMO_ORGANIZATION_TOKEN="${WORKFLOW_GITHUB_DEMO_ORGANIZATION_TOKEN}" \
+  -e WORKFLOW_GITHUB_TEMPLATE_ORGANIZATION_TOKEN="${WORKFLOW_GITHUB_TEMPLATE_ORGANIZATION_TOKEN}" \
+  -e WORKFLOW_GITHUB_ACTIONS_TOKEN="${WORKFLOW_GITHUB_ACTIONS_TOKEN}" \
+  -e TERRAFORM_PARAMETERS_B64="${TERRAFORM_PARAMETERS_B64}" \
+  ghcr.io/octodemo/container-ansible-development:base-20210217 \
+  ./finalize_provisioning.yml \
+  -vvv
